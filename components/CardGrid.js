0438cf20827fd7326449e09a7211458127732323
Julia Sartori <juliasartori@outlook.com.br>
2023-06-14 23:59:00 +0200

    refactors style for cardgrid on cardgrid module

--- components/CardGrid.js
+++ components/CardGrid.js
@@ -2,53 +2,28 @@ import React from 'react';
 import { Card } from 'primereact/card';
 import { DataView } from 'primereact/dataview';
 import Link from 'next/link';
-import classNames from 'classnames';
-
-
-
+import styles from './CardGrid.module.css';
 
 const CardGrid = ({ news }) => {
   const itemTemplate = (item) => {
-
-
     return (
-      <div
-        className="p-col-12 p-sm-6 p-md-4 p-lg-3"
-        style={{
-          marginBottom: '1rem',
-          width: '100%',
-          maxWidth: '300px',
-          display: 'flex',
-          flexDirection: 'column',
-        }}
-      >
-         <Link href="/[id]" as={`/${item.id}`} passHref>
-          <Card
-            title={item.title}
-            className={classNames('card-equal-height', 'p-m-auto')}
-            style={{
-              flexGrow: 1,
-            }}
-          >
-            <div style={{ textAlign: 'center' }}>{item.id}</div>
+      <div className={`${styles.cardGridWrapper} p-col-12 p-sm-6 p-md-4 p-lg-3`}>
+        <Link href="/[id]" as={`/${item.id}`} passHref>
+          <Card className={styles.card}>
+            <div className={styles.cardTitle}>{item.title}</div>
+            <div className={styles.cardContent}>{item.content}</div>
           </Card>
-         </Link>
+        </Link>
       </div>
     );
   };
 
   return (
-    <div className="p-d-flex p-jc-center p-ai-center" style={{ height: '100vh' }}>
+    <div className={styles.cardGridContainer}>
       <div>
         <h1>Card Grid</h1>
         <div className="p-grid">
-          <DataView
-            value={news}
-            itemTemplate={itemTemplate}
-            layout="grid"
-            rows={20}
-            paginator
-          />
+          <DataView value={news} itemTemplate={itemTemplate} layout="grid" rows={20} paginator />
         </div>
       </div>
     </div>
