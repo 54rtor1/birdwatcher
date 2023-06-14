9f43914d7af8555132706dc5c1570392c6c72bb2
Julia Sartori <juliasartori@outlook.com.br>
2023-06-14 23:38:12 +0200

    clickable detail page linked to id

--- components/CardGrid.js
+++ components/CardGrid.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { Card } from 'primereact/card';
 import { DataView } from 'primereact/dataview';
+import Link from 'next/link';
+import classNames from 'classnames';
+
+
+
 
 const CardGrid = ({ news }) => {
   const itemTemplate = (item) => {
+
+
     return (
       <div
         className="p-col-12 p-sm-6 p-md-4 p-lg-3"
@@ -15,30 +22,34 @@ const CardGrid = ({ news }) => {
           flexDirection: 'column',
         }}
       >
-        <Card
-          title={item.title}
-          className="card-equal-height"
-          style={{
-            flexGrow: 1,
-          }}
-        >
-          <div style={{ textAlign: 'center' }}>{item.id}</div>
-        </Card>
+         <Link href="/[id]" as={`/${item.id}`} passHref>
+          <Card
+            title={item.title}
+            className={classNames('card-equal-height', 'p-m-auto')}
+            style={{
+              flexGrow: 1,
+            }}
+          >
+            <div style={{ textAlign: 'center' }}>{item.id}</div>
+          </Card>
+         </Link>
       </div>
     );
   };
 
   return (
-    <div>
-      <h1>Card Grid</h1>
-      <div className="p-grid">
-        <DataView
-          value={news}
-          itemTemplate={itemTemplate}
-          layout="grid"
-          rows={20}
-          paginator
-        />
+    <div className="p-d-flex p-jc-center p-ai-center" style={{ height: '100vh' }}>
+      <div>
+        <h1>Card Grid</h1>
+        <div className="p-grid">
+          <DataView
+            value={news}
+            itemTemplate={itemTemplate}
+            layout="grid"
+            rows={20}
+            paginator
+          />
+        </div>
       </div>
     </div>
   );
---
+++ pages/[id].js
@@ -0,0 +1,11 @@
+const CardDetails = ({ id }) => {
+  return (
+    <div>
+      <h1>Card Details</h1>
+      <p>Card ID: {id}</p>
+      {/* Add more details about the card */}
+    </div>
+  );
+};
+
+export default CardDetails;
