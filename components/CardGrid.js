f540141f0709ad1ac0ce5f72c51997fd0e0f9042
Julia Sartori <juliasartori@outlook.com.br>
2023-06-14 23:04:51 +0200

    adds sample api to display

--- components/CardGrid.js
+++ components/CardGrid.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import { Card } from 'primereact/card';
+import { DataView, DataViewLayoutOptions } from 'primereact/dataview';
+import { Button } from 'primereact/button';
 
 const CardGrid = ({ news }) => {
+  const itemTemplate = (item) => {
+    return (
+      <Card title={item.title}>
+        <div className="p-grid p-nogutter">
+          <div className="p-col-12">{item.id}</div>
+        </div>
+      </Card>
+    );
+  };
+
   return (
-    <div className="card-grid">
-      {news.map((item) => (
-        <Card key={item.id} title={item.title}>
-          <div className="card-content">{item.content}</div>
-        </Card>
-      ))}
+    <div>
+      <h1>Card Grid</h1>
+      <DataView
+        value={news}
+        itemTemplate={itemTemplate}
+        layout="grid"
+        rows={5}
+        paginator
+      />
     </div>
   );
 };
--- pages/home.js
+++ pages/home.js
@@ -4,33 +4,27 @@ import CardGrid from '../components/CardGrid';
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 
 export default function Home() {
   const [news, setNews] = useState([]);
-  const sampleNews = [
-    { id: 1, title: 'News 1', content: 'This is news 1' },
-    { id: 2, title: 'News 2', content: 'This is news 2' },
-    { id: 3, title: 'News 3', content: 'This is news 3' }
-  ];
 
-  // useEffect(() => {
-  //   // const fetchData = async () => {
-  //   //   try {
-  //   //     const data = await getNews();
-  //   //     console.log(data);
-  //   //     setNews(data);
-  //   //   } catch (error) {
-  //   //     console.error(error);
-  //   //   }
-  //   // };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getNews();
+        setNews(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-  //   fetchData();
-  // }, []);
+    fetchData();
+  }, []);
 
   return (
     <div>
-      <h1>Card Grid</h1>
-      <CardGrid news={sampleNews} />
+      <CardGrid news={news} />
     </div>
   );
 }
