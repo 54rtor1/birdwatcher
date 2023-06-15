bb24d85750781f4a10f8380a6583b75cc6421547
Julia Sartori <juliasartori@outlook.com.br>
2023-06-15 02:56:44 +0200

    connects worldnews api

--- api/news.js
+++ api/news.js
@@ -3,17 +3,13 @@ import axios from 'axios';
 export async function getNews() {
   const options = {
     method: 'GET',
-    url: 'https://lgbtq-world-news-live.p.rapidapi.com/news',
-    headers: {
-      'X-RapidAPI-Key': '0e6cd512b4msh7adad42bb3ac2e4p1744ccjsn90f2d2093d8d',
-      'X-RapidAPI-Host': 'lgbtq-world-news-live.p.rapidapi.com'
-    }
+    url: 'https://api.worldnewsapi.com/search-news?api-key=d386d30dbd874b188b225b8c1cd4e8c1&text=lgbt&min-sentimen=1https://api.worldnewsapi.com/search-news?api-key=d386d30dbd874b188b225b8c1cd4e8c1&text=lgbt&min-sentimen=1&number=40',
   };
 
   try {
     const response = await axios.request(options);
-    console.log(response.data);
-    return response.data;
+    console.log(response.data.news);
+    return response.data.news;
   } catch (error) {
     console.error(error);
   }
